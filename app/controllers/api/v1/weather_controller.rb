d5ab00e61aec73c5aaf1edcddb29371ffd0eab85
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-24 18:23:37 -0400

    Create weather service class

--- app/controllers/api/v1/weather_controller.rb
+++ app/controllers/api/v1/weather_controller.rb
@@ -2,8 +2,8 @@ class Api::V1::WeatherController < ApplicationController
 
   def forecast
     coordinates = MapQuestService.coordinates(params[:location])
-    binding.pry
     weather_info = WeatherService.one_call(coordinates)
+    # binding.pry
     # search_key_word = weather_info[:search_key_word]
     photo = PhotoService.search(params[:city])
   end
