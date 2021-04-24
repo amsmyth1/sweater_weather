ba0d029d3b2a1f519e11f189059bbaf471880896
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-24 14:14:47 -0400

    Create factories. Add spec test for weather information forcasts controller

---
+++ spec/factories/location.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :location do
+    lat { "39.738453" }
+    lng { "-104.984853" }
+    city { "Denver" }
+  end
+end
---
+++ spec/requests/api/v1/weather_request_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe "Weather API Endpoints" do
+  describe "weather happy path" do
+    it "returns" do
+      get "/api/v1/forecast"
+      expect(response).to be_successful
+      weather = JSON.parse(response.body, symbolize_names:true)
+
+      expect(weather).to be_a(Hash)
+      expect(weather[:data]).to be_a(Hash)
+      expect(weather[:data].keys).to eq([:id, :type, :attributes])
+      expect(weather[:data][:type]).to eq("forecast")
+      expect(weather[:data][:attributes]).to be_a(Hash)
+      expect(weather[:data][:attributes].keys).to eq([:current_weather, :daily_weather, :hourly_weather])
+      expect(weather[:data][:attributes][:current_weather]).to be_a(Hash)
+      expect(weather[:data][:attributes][:current_weather].keys).to eq([:dateime, :temperature, :sunrise, :sunset, :feels_like, :humidity, :uvi, :visibility, :conditions, :icon])
+      expect(weather[:data][:attributes][:current_weather][:temperature]).to be_a(Float)
+      expect(weather[:data][:attributes][:current_weather][:feels_like]).to be_a(Float)
+      expect(weather[:data][:attributes][:current_weather][:humidity]).to be_a(Float)
+      expect(weather[:data][:attributes][:current_weather][:uvi]).to be_a(Float)
+      expect(weather[:data][:attributes][:current_weather][:visibility]).to be_a(Float)
+      expect(weather[:data][:attributes][:daily_weather]).to be_an(Array)
+      expect(weather[:data][:attributes][:daily_weather].count).to eq(5)
+      expect(weather[:data][:attributes][:daily_weather].first).to be_a(Hash)
+      expect(weather[:data][:attributes][:daily_weather].first.keys).to eq([:date, :sunrise, :sunset, :max_temp, :min_temp, :conditions, :icon])
+      expect(weather[:data][:attributes][:hourly_weather]).to be_an(Array)
+      expect(weather[:data][:attributes][:hourly_weather].count).to eq(8)
+      expect(weather[:data][:attributes][:hourly_weather].first).to be_a(Hash)
+      expect(weather[:data][:attributes][:hourly_weather].first.keys).to eq([:time, :temperature, :conditions, :icon])
+      expect(weather[:data][:attributes][:hourly_weather].first[:time]).to eq("14:00:00")
+
+
+    end
+  end
+  describe "photo" do
+    it "sends a photo for the page" do
+
+    end
+  end
+end
