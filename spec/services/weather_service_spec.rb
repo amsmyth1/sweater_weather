38ee56ec57e238edcd72413e585be6d5fb62a337
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-24 19:49:56 -0400

    Remove unused test

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -26,13 +26,4 @@ RSpec.describe "WeatherService", type: :feature do
 
     end
   end
-  describe "::current_weather_data" do
-    it "should return the desired weather information when given coordinates" do
-      coords = {:lat=>39.738453, :lng=>-104.984853}
-      info = WeatherService.get_city_info(coords)
-
-      expect(info.class).to eq(Hash)
-      expect(info.keys).to eq([:lat, :lon, :timezone, :timezone_offset, :current, :hourly, :daily])
-    end
-  end
 end
