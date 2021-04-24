ea2144ba8f27ea8e3c1e7085561153d6a9e3887a
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-24 14:18:29 -0400

    Add 2 endpoint routes for the weather wireframe

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/api/v1/forecast", to: "weather#forecast"
+  get "api/v1/backgrounds", to:"weather#photo"
 end
