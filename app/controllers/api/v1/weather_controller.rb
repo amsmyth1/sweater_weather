8fbe6d03d52408fa5d9e1e0dfae8dd0349712e85
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-24 16:20:10 -0400

    Add variabales that will be generated from API service objects

--- app/controllers/api/v1/weather_controller.rb
+++ app/controllers/api/v1/weather_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::WeatherController < ApplicationController
 
   def forecast
+    coordinates = MapQuestService.coordinates(params[:location])
+    weather_info = WeatherService.one_call(coordinates)
+    # search_key_word = weather_info[:search_key_word]
+    photo = PhotoService.search(params[:city])
   end
 
   def photo
