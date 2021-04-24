4500e42af6edc37d7a6038203032012a0d5700a0
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-24 13:41:14 -0400

    Correct describe typos

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
-RSpec.descsribe Location, type: :model do
+RSpec.describe Location, type: :model do
   describe "validations" do
     it { should validate_presence_of :city }
-    it { should validate_numericality_of(:lan).is_greater_than_or_equal_to(0) }
+    it { should validate_numericality_of(:lat).is_greater_than_or_equal_to(0) }
     it { should validate_numericality_of(:lng).is_greater_than_or_equal_to(0) }
   end
-  descsribe "class methods" do
-    descsribe "::" do
+  describe "class methods" do
+    describe "::" do
       it "text" do
 
       end
     end
   end
-  descsribe "instance methods" do
-    descsribe "#" do
+  describe "instance methods" do
+    describe "#" do
       it "text" do
 
       end
