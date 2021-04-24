9cff843b7f490929f3c1abd55f6f56cc6db92e80
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-24 18:42:16 -0400

    Add test for current_weather_datae

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -10,4 +10,13 @@ RSpec.describe "WeatherService", type: :feature do
       expect(info.keys).to eq([:lat, :lon, :timezone, :timezone_offset, :current, :hourly, :daily])
     end
   end
+  describe "::current_weather_data" do
+    it "should return the desired weather information when given coordinates" do
+      coords = {:lat=>39.738453, :lng=>-104.984853}
+      info = WeatherService.get_city_info(coords)
+
+      expect(info.class).to eq(Hash)
+      expect(info.keys).to eq([:lat, :lon, :timezone, :timezone_offset, :current, :hourly, :daily])
+    end
+  end
 end
