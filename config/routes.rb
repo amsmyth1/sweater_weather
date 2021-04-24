f3c8b866dc6818f6dde5f84c1685ce40cee64974
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-24 16:05:27 -0400

    Update routes to have nested controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 
-  get "/api/v1/forecast", to: "weather#forecast"
-  get "api/v1/backgrounds", to:"weather#photo"
+  get "/api/v1/forecast", to: "api/v1/weather#forecast"
+  get "api/v1/backgrounds", to:"api/v1/weather#photo"
 end
