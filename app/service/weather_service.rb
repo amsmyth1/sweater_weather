b28419f4dda526199b8bc5112599556d5711a20c
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-24 18:31:55 -0400

    Add weather data method

--- app/service/weather_service.rb
+++ app/service/weather_service.rb
@@ -1,2 +1,8 @@
 class WeatherService
-end 
+
+  def self.get_city_info(coords)
+    response = Faraday.get("https://api.openweathermap.org/data/2.5/onecall?lat=#{coords[:lat]}&lon=#{coords[:lng]}&exclude=alerts,minutely&appid=#{ENV['weather_api_key']}&units=imperial")
+    JSON.parse(response.body, symbolize_names: true)
+    binding.pry
+  end
+end
