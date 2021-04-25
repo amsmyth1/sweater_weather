90749244144115e2d7627ee10410bc3e5a7bdaa9
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-25 13:16:50 -0400

    Add edge case logic for empty coordinates to default to denver

--- app/service/map_quest_service.rb
+++ app/service/map_quest_service.rb
@@ -3,17 +3,17 @@ class MapQuestService
   def self.coordinates(location)
     response = Faraday.get("http://www.mapquestapi.com/geocoding/v1/address?key=#{ENV['mapquest_api_key']}&location=#{location}")
     raw_coordinate_data = JSON.parse(response.body, symbolize_names: true)
-    coordinates = raw_coordinate_data[:results].first[:locations].first[:latLng]
-    # location_data = {
-    #   lat: coordinates[:lat],
-    #   lng: coordinates[:lng],
-    #   city: "#{raw_coordinate_data[:results].first[:locations].first[:adminArea5]}",
-    #   state: "#{raw_coordinate_data[:results].first[:locations].first[:adminArea5]}"}
-    # Location.create(location_data)
+    if raw_coordinate_data[:info][:statuscode] == 400
+      coordinates = {:lat=>39.738453, :lng=>-104.984853}
+    else
+      coordinates = raw_coordinate_data[:results].first[:locations].first[:latLng]
+      # location_data = {
+      #   lat: coordinates[:lat],
+      #   lng: coordinates[:lng],
+      #   city: "#{raw_coordinate_data[:results].first[:locations].first[:adminArea5]}",
+      #   state: "#{raw_coordinate_data[:results].first[:locations].first[:adminArea5]}"}
+      # Location.create(location_data)
+    end
     coordinates
-  end
-
-  def self.connection
-    Faraday.new(url: ENV['http://www.mapquestapi.com/geocoding/v1/address'])
   end
 end
