8905a0289d057c31db71b3c8218a31665b6b0d02
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-25 15:25:39 -0400

    Add sad path test to user registration

--- spec/requests/api/v1/users_registration_spec.rb
+++ spec/requests/api/v1/users_registration_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe "User Registration API Endpoint" do
     it "creates and new user and returns their email and new api key when they are successfully registered" do
       user_params = ({
                       email: "host_eamil@domain.com",
-                      password_digest: "password"
+                      password_digest: "password",
+                      password_confirmation: "password"
                     })
       headers = {"CONTENT_TYPE" => "application/json"}
 
@@ -13,7 +14,25 @@ RSpec.describe "User Registration API Endpoint" do
       created_user = User.last
 
       expect(response).to be_successful
+      expect(response.status).to eq(201)
+      expect(response.body).to eq(1)
       expect(created_user.email).to eq(user_params[:email])
+    end
+  end
+  xdescribe "user registration sad path" do
+    it "creates and new user and returns their email and new api key when they are successfully registered" do
+      user_params = ({
+                      email: "host_eamil@domain.com",
+                      password_digest: "password",
+                      password_confirmation: ""
+                    })
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
+      created_user = User.last
+
+      expect(response).to be_successful
+      expect(response.status).to eq(400)
     end
   end
 end
