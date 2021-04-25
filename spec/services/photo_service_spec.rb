58e0a74e86c06c15bd44f69819ae9c5e310823b9
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-25 10:16:44 -0400

    Create photoservice search test

---
+++ spec/services/photo_service_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "PhotoService", type: :feature do
+  describe "::search" do
+    it "returns a photo of the queried city" do
+      photos = PhotoService.search("denver,co")
+
+      expect(photos[:location]).to eq("denver,co")
+      expect(photos[:image_url]).to be_a(String)
+      expect(photos[:credit]).to be_a(Hash)
+      expect(photos[:credit].keys).to eq([:source, :author, :logo])
+    end
+  end
+end
