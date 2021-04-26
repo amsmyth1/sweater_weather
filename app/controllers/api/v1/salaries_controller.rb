09442230cfd695f3f259e92e2acab5a296d861cf
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-26 13:22:16 -0400

    Refactor from controller to facade, all tests passing

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -1,13 +1,14 @@
 class Api::V1::SalariesController < ApplicationController
 
   def city_info
-    city_info = {}
-    city_info[:destination] = params[:destination]
-    city_info[:forecast] = city_forecast(params[:destination])
-    city_info[:salaries] = SalaryService.city_salary_info(params[:destination])
-    city_info
-    final_city_info = OpenStruct.new(city_info)
-    render json: SalariesSerializer.new(final_city_info)
+    # city_info = {}
+    # city_info[:destination] = params[:destination]
+    # city_info[:forecast] = city_forecast(params[:destination])
+    # city_info[:salaries] = SalaryService.city_salary_info(params[:destination])
+    # city_info
+    # final_city_info = OpenStruct.new(city_info)
+
+    render json: SalariesSerializer.new(SalariesFacade.final_city_info(params[:destination]))
   end
 
   # def city_salary_info(location)
@@ -44,18 +45,18 @@ class Api::V1::SalariesController < ApplicationController
   #   salary_info
   # end
 
-  def city_forecast(city)
-    response = Faraday.get("https://api.teleport.org/api/urban_areas/slug:#{params[:destination]}/")
-    raw_data = JSON.parse(response.body, symbolize_names: true)
-    coords = MapQuestService.coordinates(raw_data[:full_name])
-
-    response = Faraday.get("https://api.openweathermap.org/data/2.5/onecall?lat=#{coords[:lat]}&lon=#{coords[:lng]}&exclude=alerts,minutely&appid=#{ENV['weather_api_key']}&units=imperial")
-    raw_data = JSON.parse(response.body, symbolize_names: true)
-    forecast = {}
-    forecast[:summary] = raw_data[:current][:weather].first[:description]
-    forecast[:temperature] = raw_data[:current][:temp]
-    forecast
-  end
+  # def city_forecast(city)
+  #   response = Faraday.get("https://api.teleport.org/api/urban_areas/slug:#{params[:destination]}/")
+  #   raw_data = JSON.parse(response.body, symbolize_names: true)
+  #   coords = MapQuestService.coordinates(raw_data[:full_name])
+  #
+  #   response = Faraday.get("https://api.openweathermap.org/data/2.5/onecall?lat=#{coords[:lat]}&lon=#{coords[:lng]}&exclude=alerts,minutely&appid=#{ENV['weather_api_key']}&units=imperial")
+  #   raw_data = JSON.parse(response.body, symbolize_names: true)
+  #   forecast = {}
+  #   forecast[:summary] = raw_data[:current][:weather].first[:description]
+  #   forecast[:temperature] = raw_data[:current][:temp]
+  #   forecast
+  # end
 
   # def present_salary(salary)
   #   final_salary = []
--- app/facades/salaries_facade.rb
+++ app/facades/salaries_facade.rb
@@ -1,2 +1,24 @@
 class SalariesFacade
-end 
+
+  def self.final_city_info(location)
+    city_info = {}
+    city_info[:destination] = location
+    city_info[:forecast] = city_forecast(location)
+    city_info[:salaries] = SalaryService.city_salary_info(location)
+    city_info
+    final_city_info = OpenStruct.new(city_info)
+  end
+
+  def self.city_forecast(location)
+    response = Faraday.get("https://api.teleport.org/api/urban_areas/slug:#{location}/")
+    raw_data = JSON.parse(response.body, symbolize_names: true)
+    coords = MapQuestService.coordinates(raw_data[:full_name])
+
+    response = Faraday.get("https://api.openweathermap.org/data/2.5/onecall?lat=#{coords[:lat]}&lon=#{coords[:lng]}&exclude=alerts,minutely&appid=#{ENV['weather_api_key']}&units=imperial")
+    raw_data = JSON.parse(response.body, symbolize_names: true)
+    forecast = {}
+    forecast[:summary] = raw_data[:current][:weather].first[:description]
+    forecast[:temperature] = raw_data[:current][:temp]
+    forecast
+  end
+end
