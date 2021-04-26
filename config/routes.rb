dd10581d4e8c72aa33487513db1f8ef02effb03d
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-26 12:35:16 -0400

    Add route for endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   get "/api/v1/forecast", to: "api/v1/weather#forecast"
-  get "api/v1/backgrounds", to:"api/v1/weather#photo"
-  post "api/v1/users", to:"api/v1/users#register"
+  get "/api/v1/backgrounds", to:"api/v1/weather#photo"
+  post "/api/v1/users", to:"api/v1/users#register"
+
+  get "/api/v1/salaries", to:"api/v1/salaries#city_info"
 end
