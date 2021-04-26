6e5538414e83d0093845aad1fbfd1cd7fdafd706
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-26 13:47:20 -0400

    Add more happy path tests

--- spec/requests/api/v1/salaries_request_spec.rb
+++ spec/requests/api/v1/salaries_request_spec.rb
@@ -25,6 +25,52 @@ RSpec.describe "Urban Area Salaries API Endpoint" do
       expect(philly_info[:data][:attributes][:forecast]).to be_a(Hash)
       expect(philly_info[:data][:attributes][:forecast].keys).to eq([:summary, :temperature])
     end
+    it "should return a city name, forecast, and salaries " do
+      get "/api/v1/salaries?destination=denver"
+      expect(response).to be_successful
+      philly_info = JSON.parse(response.body, symbolize_names:true)
+
+      expect(philly_info).to be_a(Hash)
+      expect(philly_info[:data]).to be_a(Hash)
+      expect(philly_info[:data].keys).to eq([:id, :type, :attributes])
+      expect(philly_info[:data][:type]).to eq("salaries")
+      expect(philly_info[:data][:attributes]).to be_a(Hash)
+      expect(philly_info[:data][:attributes].keys).to eq([:destination, :forecast, :salaries])
+      expect(philly_info[:data][:attributes][:destination]).to eq("denver")
+
+      expect(philly_info[:data][:attributes][:salaries]).to be_an(Array)
+      expect(philly_info[:data][:attributes][:salaries].count).to be_between(0, 7).inclusive
+      expect(philly_info[:data][:attributes][:salaries].first).to be_a(Hash)
+      expect(philly_info[:data][:attributes][:salaries].first.keys).to eq([:title, :min, :max])
+      expect(philly_info[:data][:attributes][:salaries].first[:title]).to be_a(String)
+      expect(philly_info[:data][:attributes][:salaries].first[:min]).to be_a(String)
+      expect(philly_info[:data][:attributes][:salaries].first[:max]).to be_a(String)
+      expect(philly_info[:data][:attributes][:forecast]).to be_a(Hash)
+      expect(philly_info[:data][:attributes][:forecast].keys).to eq([:summary, :temperature])
+    end
+    it "should return a city name, forecast, and salaries " do
+      get "/api/v1/salaries?destination=chicago"
+      expect(response).to be_successful
+      philly_info = JSON.parse(response.body, symbolize_names:true)
+
+      expect(philly_info).to be_a(Hash)
+      expect(philly_info[:data]).to be_a(Hash)
+      expect(philly_info[:data].keys).to eq([:id, :type, :attributes])
+      expect(philly_info[:data][:type]).to eq("salaries")
+      expect(philly_info[:data][:attributes]).to be_a(Hash)
+      expect(philly_info[:data][:attributes].keys).to eq([:destination, :forecast, :salaries])
+      expect(philly_info[:data][:attributes][:destination]).to eq("chicago")
+
+      expect(philly_info[:data][:attributes][:salaries]).to be_an(Array)
+      expect(philly_info[:data][:attributes][:salaries].count).to be_between(0, 7).inclusive
+      expect(philly_info[:data][:attributes][:salaries].first).to be_a(Hash)
+      expect(philly_info[:data][:attributes][:salaries].first.keys).to eq([:title, :min, :max])
+      expect(philly_info[:data][:attributes][:salaries].first[:title]).to be_a(String)
+      expect(philly_info[:data][:attributes][:salaries].first[:min]).to be_a(String)
+      expect(philly_info[:data][:attributes][:salaries].first[:max]).to be_a(String)
+      expect(philly_info[:data][:attributes][:forecast]).to be_a(Hash)
+      expect(philly_info[:data][:attributes][:forecast].keys).to eq([:summary, :temperature])
+    end
   end
   describe "sad path" do
     it "returns null if the city is not recognizable " do
