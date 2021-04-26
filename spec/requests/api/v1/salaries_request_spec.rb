d4f146e61d4caaac67e6d49be218916de898b94e
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-26 12:35:04 -0400

    Flesh out test for endpoint

--- spec/requests/api/v1/salaries_request_spec.rb
+++ spec/requests/api/v1/salaries_request_spec.rb
@@ -4,25 +4,26 @@ RSpec.describe "Urban Area Salaries API Endpoint" do
   describe "happy path" do
     it "should return a city name, forecast, and salaries " do
       get "/api/v1/salaries?destination=philadelphia"
-      exepect(response).to be_successful
+      expect(response).to be_successful
       philly_info = JSON.parse(response.body, symbolize_names:true)
 
-      expect(weather).to be_a(Hash)
-      expect(weather[:data]).to be_a(Hash)
-      expect(weather[:data].keys).to eq([:id, :type, :attributes])
-      expect(weather[:data][:type]).to eq("salaries")
-      expect(weather[:data][:attributes]).to be_a(Hash)
-      expect(weather[:data][:attributes].keys).to eq([:destination, :forecast, :salaries])
-      expect(weather[:data][:attributes][:destination]).to eq("philadelphia")
-      expect(weather[:data][:attributes][:forecast]).to be_a(Hash)
-      expect(weather[:data][:attributes][:forecast].keys).to eq([:summary, :temperature])
-      expect(weather[:data][:attributes][:salaries]).to be_an(Array)
-      expect(weather[:data][:attributes][:salaries].first).to be_a(Hash)
-      expect(weather[:data][:attributes][:salaries].first.keys).to eq([:title, :min, :max])
-      expect(weather[:data][:attributes][:salaries].first[:title]).to be_a(String)
-      expect(weather[:data][:attributes][:salaries].first[:min]).to be_a(String)
-      expect(weather[:data][:attributes][:salaries].first[:max]).to be_a(String)
-      expect(weather[:data][:attributes][:salaries].count).to is_less_than_or_equal_to(7)
+      expect(philly_info).to be_a(Hash)
+      expect(philly_info[:data]).to be_a(Hash)
+      expect(philly_info[:data].keys).to eq([:id, :type, :attributes])
+      expect(philly_info[:data][:type]).to eq("salaries")
+      expect(philly_info[:data][:attributes]).to be_a(Hash)
+      expect(philly_info[:data][:attributes].keys).to eq([:destination, :forecast, :salaries])
+      expect(philly_info[:data][:attributes][:destination]).to eq("philadelphia")
+
+      expect(philly_info[:data][:attributes][:salaries]).to be_an(Array)
+      expect(philly_info[:data][:attributes][:salaries].first).to be_a(Hash)
+      expect(philly_info[:data][:attributes][:salaries].first.keys).to eq([:title, :min, :max])
+      expect(philly_info[:data][:attributes][:salaries].first[:title]).to be_a(String)
+      expect(philly_info[:data][:attributes][:salaries].first[:min]).to be_a(String)
+      expect(philly_info[:data][:attributes][:salaries].first[:max]).to be_a(String)
+      expect(philly_info[:data][:attributes][:salaries].count).to is_less_than_or_equal_to(7)
+      expect(philly_info[:data][:attributes][:forecast]).to be_a(Hash)
+      expect(philly_info[:data][:attributes][:forecast].keys).to eq([:summary, :temperature])
     end
   end
 end
