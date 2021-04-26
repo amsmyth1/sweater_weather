56e030f7aef059214134f2ed6d606aa738a17017
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-26 12:58:17 -0400

    Complete controller methods for exposed endpoint

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -6,7 +6,8 @@ class Api::V1::SalariesController < ApplicationController
     city_info[:forecast] = city_forecast(params[:destination])
     city_info[:salaries] = city_salary_info(params[:destination])
     city_info
-    render json: SalariesSerializer.new(OpenStruct.new(city_info))
+    final_city_info = OpenStruct.new(city_info)
+    render json: SalariesSerializer.new(final_city_info)
   end
 
   def city_salary_info(location)
@@ -44,6 +45,16 @@ class Api::V1::SalariesController < ApplicationController
   end
 
   def city_forecast(city)
+    response = Faraday.get("https://api.teleport.org/api/urban_areas/slug:#{params[:destination]}/")
+    raw_data = JSON.parse(response.body, symbolize_names: true)
+    coords = MapQuestService.coordinates(raw_data[:full_name])
+
+    response = Faraday.get("https://api.openweathermap.org/data/2.5/onecall?lat=#{coords[:lat]}&lon=#{coords[:lng]}&exclude=alerts,minutely&appid=#{ENV['weather_api_key']}&units=imperial")
+    raw_data = JSON.parse(response.body, symbolize_names: true)
+    forecast = {}
+    forecast[:summary] = raw_data[:current][:weather].first[:description]
+    forecast[:temperature] = raw_data[:current][:temp]
+    forecast
   end
 
   def present_salary(salary)
