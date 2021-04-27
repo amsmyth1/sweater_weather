fa14fe397bfbe836728cd65d81212b2ace78ef89
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-27 14:11:39 -0400

    Remove commented out code

--- spec/requests/api/v1/roadtrips_request_spec.rb
+++ spec/requests/api/v1/roadtrips_request_spec.rb
@@ -16,21 +16,20 @@ RSpec.describe "Road Trip API Endpoint" do
         api_key: "#{@created_user.api_key}"
       })
       headers = {"CONTENT_TYPE" => "application/json"}
-      post "/api/v1/road_trip", headers: headers, params: JSON.generate(user_params)
+      post "/api/v1/road_trip", headers: headers, params: JSON.generate(road_trip_params)
 
       expect(response).to be_successful
-      expect(response.status).to eq(201)
       data = JSON.parse(response.body, symbolize_names: true)[:data]
 
       expect(data[:id]).to eq(nil)
       expect(data[:type]).to eq("roadtrip")
       expect(data[:attributes]).to be_a(Hash)
-      expect(data[:attributes].keys).to be_a([:start_city, :end_city, :travel_time, :weather_at_eta])
-      expect(data[:attributes][:start_city]).to eq(road_trip_params[:orgin])
+      expect(data[:attributes].keys).to eq([:start_city, :end_city, :travel_time, :weather_at_eta])
+      expect(data[:attributes][:start_city]).to eq(road_trip_params[:origin])
       expect(data[:attributes][:end_city]).to eq(road_trip_params[:destination])
       expect(data[:attributes][:travel_time]).to be_a(String)
       expect(data[:attributes][:weather_at_eta]).to be_a(Hash)
-      expect(data[:attributes][:weather_at_eta].keys).to be_a([:temperature, :conditions])
+      expect(data[:attributes][:weather_at_eta].keys).to eq([:temperature, :conditions])
     end
   end
 end
