0d573bffa7bee411b7f58bc9afad7b9f3775b9a9
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-27 14:12:31 -0400

    Add serializer and private method for creating openstruct object

--- app/controllers/api/v1/road_trips_controller.rb
+++ app/controllers/api/v1/road_trips_controller.rb
@@ -1,5 +1,24 @@
 class Api::V1::RoadTripsController < ApplicationController
 
   def create
-  end 
+    render json: RoadTripSerializer.new(create_roadtrip(params))
+  end
+
+  private
+
+  def create_roadtrip(params)
+    road_trip = {}
+    road_trip[:start_city] = params[:origin]
+    road_trip[:end_city] = params[:destination]
+    road_trip[:travel_time] = trip_duration(params[:origin], params[:destination])
+    road_trip[:weather_at_eta] = {}
+    OpenStruct.new(road_trip)
+  end
+
+  def trip_duration(origin, destination)
+    response = Faraday.get("http://www.mapquestapi.com/directions/v2/route?key=#{ENV['mapquest_api_key']}&from=#{origin}&to=#{destination}")
+    raw_data = JSON.parse(response.body, symbolize_names: true)
+    time = raw_data[:route][:formattedTime]
+    "#{time[0,2]} hours, #{time[3,2]} minutes"
+  end
 end
